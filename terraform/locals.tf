9d762a7b0648e433f2716d36f29e6d90ee39c314
Nguyễn Quý Ngọc <76089776+azzo-dude@users.noreply.github.com>
2025-07-26 01:10:50 +0700

    Create locals.tf

---
+++ terraform/locals.tf
@@ -0,0 +1,19 @@
+# --- GLOBAL VARIABLES (LOCALS) ---
+# Define all your common settings here for easy access
+locals {
+  # --- Proxmox API Credentials ---
+  # WARNING: Hardcoding secrets is a security risk!
+  api_token_id     = "terraform-user@pve!terraform-token"
+  api_token_secret = "YOUR_API_SECRET_HERE" // IMPORTANT: Change this!
+
+  # --- LXC Container Settings ---
+  target_node      = "pve"
+  template_storage = "local"
+  template_name    = "vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
+  disk_storage     = "local-lvm"
+  root_password    = "YOUR_TEMPLATE_ROOT_PASSWORD" // IMPORTANT: Change this!
+
+  # --- Hostnames ---
+  master_hostname  = "LXC-MASTER"
+  network_hostname = "LXC-NETWORK"
+}
