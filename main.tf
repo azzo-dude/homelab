d1dd4096274e2e5c1af7a538e0f8c903426c9410
Nguyễn Quý Ngọc <76089776+azzo-dude@users.noreply.github.com>
2025-07-26 01:03:08 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,3 @@
-# main.tf
-
 terraform {
   required_providers {
     proxmox = {
@@ -10,37 +8,109 @@ terraform {
 }
 
 provider "proxmox" {
-  pm_api_url          = "https://127.0.0.1:8006/api2/json" // Use localhost IP since you're on the server
+  pm_api_url          = "https://127.0.0.1:8006/api2/json"
   pm_api_token_id     = "terraform-user@pve!terraform-token"
   pm_api_token_secret = var.proxmox_api_token_secret
   pm_tls_insecure     = true
 }
 
-# --- ADD THIS RESOURCE BLOCK ---
-resource "proxmox_vm_qemu" "example_vm" {
-  # --- General Settings ---
-  name        = "my-first-terraform-vm"
-  desc        = "Created with Terraform"
+# --- LXC Master Node ---
+resource "proxmox_lxc" "lxc_master" {
+  hostname    = "LXC-MASTER"
+  vmid        = 100
   target_node = "pve" // IMPORTANT: Change to your Proxmox node's name
 
-  # --- Template and OS ---
-  clone = "ubuntu-2204-cloudinit-template" // IMPORTANT: Change to your template name
-  agent = 1                                // Enable the QEMU guest agent
-  
-  # --- Hardware ---
-  cores  = 1
-  memory = 1024
-
-  # --- Networking ---
-  network {
-    model  = "virtio"
-    bridge = "vmbr0"
+  // --- Template & Storage ---
+  ostemplate = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst" // IMPORTANT: Change to your container template
+  rootfs {
+    storage = "local-lvm" // IMPORTANT: Change to your storage pool
+    size    = "8G"
   }
 
-  # --- Storage ---
-  disk {
-    storage = "local-lvm" // IMPORTANT: Change to your storage pool name
-    size    = "10G"
-    type    = "scsi"
+  // --- Resources ---
+  memory = 1024
+  cores  = 1
+
+  // --- Options ---
+  start        = true // Start on boot
+  unprivileged = true
+  features {
+    keyctl = true
+  }
+
+  // --- Networking ---
+  // Ensure your template is configured for DHCP or set a static IP.
+  network {
+    name   = "eth0"
+    bridge = "vmbr0"
+    ip     = "dhcp"
+  }
+
+  // --- Automation: Update on create ---
+  // This requires the container to have SSH enabled and a known user/password.
+  // Ensure your template has an SSH server and a user (e.g., 'root').
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'Waiting for cloud-init to finish...'",
+      "sleep 15", // Give the container time to boot and get an IP
+      "apt-get update",
+      "apt-get upgrade -y"
+    ]
+
+    connection {
+      type     = "ssh"
+      user     = "root"
+      password = "YOUR_TEMPLATE_ROOT_PASSWORD" // IMPORTANT: Change this!
+      host     = self.ssh_host
+    }
+  }
+}
+
+# --- LXC Network Node ---
+resource "proxmox_lxc" "lxc_network" {
+  hostname    = "LXC-NETWORK"
+  vmid        = 101
+  target_node = "pve" // IMPORTANT: Change to your Proxmox node's name
+
+  // --- Template & Storage ---
+  ostemplate = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst" // IMPORTANT: Change to your container template
+  rootfs {
+    storage = "local-lvm" // IMPORTANT: Change to your storage pool
+    size    = "8G"
+  }
+
+  // --- Resources ---
+  memory = 1024
+  cores  = 1
+
+  // --- Options ---
+  start        = true // Start on boot
+  unprivileged = true
+  features {
+    keyctl = true
+  }
+
+  // --- Networking ---
+  network {
+    name   = "eth0"
+    bridge = "vmbr0"
+    ip     = "dhcp"
+  }
+
+  // --- Automation: Update on create ---
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'Waiting for cloud-init to finish...'",
+      "sleep 15",
+      "apt-get update",
+      "apt-get upgrade -y"
+    ]
+
+    connection {
+      type     = "ssh"
+      user     = "root"
+      password = "YOUR_TEMPLATE_ROOT_PASSWORD" // IMPORTANT: Change this!
+      host     = self.ssh_host
+    }
   }
 }
